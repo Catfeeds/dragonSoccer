2e7edc1f5d0400b6dabd6f6430b20a6043f11aeb
xinyezaici <jxsshxy@126.com>
2018-03-23 18:54:47 +0800

    webhook

--- public/hook/deploy.php
+++ public/hook/deploy.php
@@ -1,5 +1,5 @@
 <?php #!/usr/bin/env /usr/bin/php100dd
-    error_reporting(E_ALL);
+/*    error_reporting(E_ALL);
     ini_set('display_errors', '1');
     set_time_limit(0);
 	 
@@ -22,4 +22,22 @@
 		file_put_contents('/alidata/www/dragonSoccer/logs/github.txt', $output, FILE_APPEND);
 										      
 	}
+*/
+	$secret = "xinyezaici";
+	// 项目根目录, 如: "/var/www/fizzday"
+	$path = "/alidata/www/dragonSoccer";
+	// Headers deliveried from GitHub
+	$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+	if ($signature) {
+	    $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+	    if (strcmp($signature, $hash) == 0) {
+	        echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+	        exit();
+	    }else{
+	    	file_put_contents('/alidata/www/dragonSoccer/logs/github.txt', true);
+	    }	
+	}else{
+	    file_put_contents('/alidata/www/dragonSoccer/logs/github.txt', true);
+	}
+	http_response_code(404);
 ?>
